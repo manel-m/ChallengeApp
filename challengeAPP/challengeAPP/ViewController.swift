7e5e7eede4dd9e401e109950e9c0d14988ae5f4b
Manel Matougui <m.matougui@gmail.com>
2018-02-21 16:25:04 -0800

    UISwitch Button

--- challengeAPP/challengeAPP/ViewController.swift
+++ challengeAPP/challengeAPP/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     let zipCodeDelegate = ZipCodeTextFieldDelegate ()
     @IBOutlet weak var textField2: UITextField!
@@ -20,12 +20,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.textField2.delegate = zipCodeDelegate
+        self.textField3.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
+        self.editingSwitch.setOn(false, animated: false)
+    }
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return self.editingSwitch.isOn
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func toggleTheTextEditor(_ sender: AnyObject) {
+        
+        if !(sender as! UISwitch).isOn {
+            self.textField3.resignFirstResponder()
+        }
     }
 
 
